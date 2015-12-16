b6ea60fa171cd3dcb0bc2ea939a966d0e0221e9d
Martin Wildfeuer <mwfire@mwfire.de>
2015-12-16 22:19:01 +0100

    Added team section header

---
+++ Teams-Up/TeamSectionTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TeamSectionTableViewCell.swift
+//  Teams-Up
+//
+//  Created by Martin Wildfeuer on 16.12.15.
+//  Copyright © 2015 Jhoan Arango. All rights reserved.
+//
+
+import UIKit
+
+class TeamSectionTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var starRating: CosmosView!
+}
